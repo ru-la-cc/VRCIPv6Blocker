12b91c83460d21a5e6afe0fa42c3135d858d8197
Zungrammer <43912963+ru-la-cc@users.noreply.github.com>
2025-08-18 23:37:53 +0900

    ComPtr統一したはずだからぶいちゃインするか@

--- VRCIPv6Blocker/src/ipv6conf.cpp
+++ VRCIPv6Blocker/src/ipv6conf.cpp
@@ -1,5 +1,5 @@
 ﻿#include "ipv6conf.h"
-
+#include "ydkcomptr.h"
 #include <netioapi.h>
 #include <iphlpapi.h>
 #include <initguid.h>
@@ -18,30 +18,29 @@ namespace ydk {
 		return ifIndex != 0 || std::memcmp(&ifGuid, &zero, sizeof(GUID)) != 0;
 	}
 
-	// これそのうち共通化する
 	namespace {
-		template<class T>
-		class ComPtr {
-			T* p_ = nullptr;
-		public:
-			ComPtr() = default;
-			explicit ComPtr(T* p) : p_(p) {}
-			ComPtr(const ComPtr&) = delete;
-			ComPtr& operator=(const ComPtr&) = delete;
-			ComPtr(ComPtr&& o) noexcept : p_(o.p_) { o.p_ = nullptr; }
-			ComPtr& operator=(ComPtr&& o) noexcept {
-				if (this != &o) { reset(); p_ = o.p_; o.p_ = nullptr; }
-				return *this;
-			}
-			~ComPtr() { reset(); }
+		//template<class T>
+		//class ComPtr {
+		//	T* p_ = nullptr;
+		//public:
+		//	ComPtr() = default;
+		//	explicit ComPtr(T* p) : p_(p) {}
+		//	ComPtr(const ComPtr&) = delete;
+		//	ComPtr& operator=(const ComPtr&) = delete;
+		//	ComPtr(ComPtr&& o) noexcept : p_(o.p_) { o.p_ = nullptr; }
+		//	ComPtr& operator=(ComPtr&& o) noexcept {
+		//		if (this != &o) { reset(); p_ = o.p_; o.p_ = nullptr; }
+		//		return *this;
+		//	}
+		//	~ComPtr() { reset(); }
 
-			T* get()  const noexcept { return p_; }
-			T** put()        noexcept { reset(); return &p_; } // 受け渡し用
-			T* detach()     noexcept { T* t = p_; p_ = nullptr; return t; }
-			void reset(T* p = nullptr) noexcept { if (p_) p_->Release(); p_ = p; }
-			T* operator->() const noexcept { return p_; }
-			explicit operator bool() const noexcept { return p_ != nullptr; }
-		};
+		//	T* get()  const noexcept { return p_; }
+		//	T** put()        noexcept { reset(); return &p_; } // 受け渡し用
+		//	T* detach()     noexcept { T* t = p_; p_ = nullptr; return t; }
+		//	void reset(T* p = nullptr) noexcept { if (p_) p_->Release(); p_ = p; }
+		//	T* operator->() const noexcept { return p_; }
+		//	explicit operator bool() const noexcept { return p_ != nullptr; }
+		//};
 
 		// IP 文字列パース（IPv4/IPv6）
 		inline bool ParseIpLiteral(LPCWSTR ip, sockaddr_storage& ss, int& family) noexcept {
