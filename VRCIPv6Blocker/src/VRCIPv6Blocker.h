54fa2f2f11ec7277530580a2dded08313bdf6a28
Zungrammer <43912963+ru-la-cc@users.noreply.github.com>
2025-08-24 17:42:54 +0900

    VRChat.exeを登録する準備的なのはできたけど一応ベータ2としようと思う

--- VRCIPv6Blocker/src/VRCIPv6Blocker.h
+++ VRCIPv6Blocker/src/VRCIPv6Blocker.h
@@ -19,15 +19,19 @@ public:
 		UINT uFirewallBlock;
 		UINT uNonBlocking;
 		UINT uRevert;
+		UINT uOnlyVRC;
 		std::wstring strExecutePath;
 		std::wstring strVRCFile;
 		std::wstring strDestIp;
 		std::wstring strNIC;
+		std::wstring strVRCFullPath;
 	};
 
 	static inline constexpr UINT WM_VRCEXIT = WM_APP + 1;
 	static inline constexpr UINT WM_SET_CTRLTEXT = WM_APP + 2;
 	static inline constexpr UINT WM_ERR_MESSAGE = WM_APP + 3;
+	static inline constexpr UINT WM_WRITE_VRCFULLPATH = WM_APP + 4;
+	static inline constexpr UINT WM_ENABLE_CONTROL = WM_APP + 5;
 
 	static constexpr LPCWSTR REGISTER_NAME = APP_GUID L"_" APP_NAME;
 	LPCWSTR BLOCK_LIST_FILE = L"blocklist.txt";
@@ -38,9 +42,11 @@ public:
 	LPCWSTR IK_EXECUTEPATH = L"Execute";
 	LPCWSTR IK_NONBLOCKING = L"NonBlocking";
 	LPCWSTR IK_REVERT = L"Revert";
+	LPCWSTR IK_ONLYVRC = L"OnlyVRC";
 	LPCWSTR IK_VRCFILE = L"VRCFile";
 	LPCWSTR IK_DESTIP = L"DestIp";
 	LPCWSTR IK_NIC = L"NIC";
+	LPCWSTR IK_VRCFULLPATH = L"VRCFullPath";
 	LPCWSTR ARG_AUTORUN = L"-autorun";
 
 	virtual ~VRCIPv6BlockerApp();
@@ -61,6 +67,7 @@ private:
 	std::vector<std::wstring> m_BlockList;
 	std::wstring m_ModulePath;
 	std::wstring m_IniFile;
+	INI_SETTING m_Setting = {};
 	ydk::ComInitializer m_comInitializer;
 	CRITICAL_SECTION m_tCs;
 	CRITICAL_SECTION m_tidCs;
@@ -73,17 +80,14 @@ private:
 	LPCWSTR VRCFILENAME = L"VRChat.exe";
 	HANDLE m_hMonThread = nullptr;
 	HANDLE m_hWaitThread = nullptr;
-	int m_argc;
 	const DWORD PROCESS_MONITOR_INTERVAL = 100UL;
 	DWORD m_vrcProcessId = 0;
+	int m_argc;
 	bool m_isAutoRun = false;
 	bool m_isStop = false;
 	bool m_isFirewallBlocked = false;
 	bool m_isIPv6Enabled = true;
 	bool m_isTaskExist = false;
-
-	// 設定関連
-	INI_SETTING m_Setting = {};
 
 	static unsigned __stdcall VRCMonitoringThread(void* param);
 	static unsigned __stdcall ProcessExitNotifyThread(void* param);
@@ -118,7 +122,8 @@ private:
 	void CreateScheduledTaskWithShortcut();
 	void DeleteTask();
 
-	// ---------------------------- 以下、検討中件検証中 ...
+	void WriteExePath();
+	// ---------------------------- 以下、使ってないけど残しておく
 	std::wstring GetLinkPath(LPCWSTR lpLinkFile);
 	bool GetExeFilePath(LPCWSTR lpLaunchPath, std::wstring& exePath);
 };
