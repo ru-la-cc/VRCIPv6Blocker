ca17da5c3f9c25add743a302d98ca1699a038212
Zungrammer <43912963+ru-la-cc@users.noreply.github.com>
2026-05-02 03:19:39 +0900

    作っとくか

---
+++ VRCIPv6Blocker/src/ScopedHandle.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include <windows.h>
+
+namespace ydk {
+
+	// INVALID_HANDLE_VALUE用
+	class ScopedHandle final {
+	public:
+		ScopedHandle(HANDLE h) noexcept : m_handle(h) {}
+		~ScopedHandle() {
+			if (m_handle != INVALID_HANDLE_VALUE) ::CloseHandle(m_handle);
+			m_handle = INVALID_HANDLE_VALUE;
+		}
+		ScopedHandle(const ScopedHandle&) = delete;
+		ScopedHandle& operator=(const ScopedHandle&) = delete;
+		ScopedHandle(ScopedHandle&& other) noexcept : m_handle(other.m_handle) {
+			other.m_handle = INVALID_HANDLE_VALUE;
+		}
+		ScopedHandle& operator=(ScopedHandle&& other) noexcept {
+			if (this != &other) {
+				if (m_handle != INVALID_HANDLE_VALUE) ::CloseHandle(m_handle);
+				m_handle = other.m_handle;
+				other.m_handle = INVALID_HANDLE_VALUE;
+			}
+		}
+		HANDLE Get() const noexcept { return m_handle; }
+	private:
+		HANDLE m_handle;
+	};
+}
