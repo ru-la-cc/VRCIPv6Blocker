9d56bbecf9ead3a76a9e15e3fe97776a52423de5
MA <subdev.cw@gmail.com>
2026-05-09 15:02:28 +0900

    スレッド回りのロジックを修正・機能は特になんも変えてない

--- VRCIPv6Blocker/src/vrcprocess.h
+++ VRCIPv6Blocker/src/vrcprocess.h
@@ -19,7 +19,10 @@ public:
 	};
 	VRCProcess() = delete;
 	VRCProcess(LPCWSTR lpExeFile, ydk::ILogger<WCHAR>* logger) :
-		m_ExeFile(lpExeFile), m_hProcess(nullptr), m_ProcessID(0), m_Logger(logger) {
+		m_ExeFile(lpExeFile),
+		m_hProcess(nullptr),
+		m_ProcessID(0),
+		m_Logger(logger) {
 		::InitializeCriticalSection(&m_cs);
 		::InitializeCriticalSection(&m_execute_cs);
 	}
@@ -34,6 +37,31 @@ public:
 	VRCProcess& operator=(const VRCProcess&) = delete;
 	VRCProcess(VRCProcess&&) = delete;
 	VRCProcess& operator=(VRCProcess&&) = delete;
+	inline void MakeParams(
+		std::atomic<bool>* stopflag,
+		std::function<void(LPCWSTR)> settext,
+		std::function<void(WPARAM, LPARAM)> vrcexit,
+		CRITICAL_SECTION& cs
+	) {
+		if (m_Params) return; // ワーカースレッドと待機スレッドはそれぞれ1個しか作らないからな
+		m_Params = std::make_unique<MonitorParams>(
+			this,
+			&m_Waiter,
+			stopflag,
+			settext,
+			vrcexit,
+			cs
+		);
+	}
+	inline void StartWorker() { m_Worker.emplace(VRCMonitorThread, m_Params.get()); }
+	inline void ShutdownWorker() {
+		if (!m_Worker) return;
+		if (m_Worker.has_value()) {
+			m_Params->stopflag->store(true);
+			m_Worker.value().join();
+			m_Worker.reset();
+		}
+	}
 	DWORD GetProcessID();
 	int UserExecute(LPCWSTR lpExePath); // -1:エラー 0:起動している 1:起動した
 	static void VRCMonitorThread(MonitorParams* params);
@@ -42,8 +70,11 @@ private:
 	HANDLE m_hProcess;
 	DWORD m_ProcessID;
 	ydk::ILogger<WCHAR>* m_Logger;
+	std::unique_ptr<MonitorParams> m_Params;
 	CRITICAL_SECTION m_cs;
 	CRITICAL_SECTION m_execute_cs;
+	std::optional<std::thread> m_Worker;
+	std::optional<std::thread> m_Waiter;
 	bool FindProcess();
 	static void Waiter(MonitorParams* params);
 	static void VRCWaitThread(
